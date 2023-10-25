c631384cc106734a7edfd8f53f4f7fe56bc66007
Kacper <kacpercybulski@icloud.com>
2023-10-25 22:13:03 +0200

    will finish styling tomorrow

---
+++ components/Meme.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Meme() {
+	return (
+		<div className="form">
+			<input type="text" value="Shut up" className="first-input" />
+			<input type="text" value="and take my money" className="second-inp" />
+
+			<button className="button">Get a new meme image 🖼</button>
+		</div>
+	)
+}
+
+export default Meme
