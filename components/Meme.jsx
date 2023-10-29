423a398b17f6c06ab0ccca36150b2b78cd2cd51e
Kacper <kacpercybulski@icloud.com>
2023-10-29 22:26:15 +0100

    .

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,42 +1,26 @@
 import React from 'react'
+import memesData from '../memesData.jsx'
 
 function Meme() {
+	function getMemeImage() {
+		event.preventDefault()
+		const memesArray = memesData.data.memes
+		const randomNumber = Math.floor(Math.random() * memesArray.length)
+
+		const url = memesArray[randomNumber].url
+
+		console.log(url)
+	}
+
 	return (
 		<main>
-		
-
 			<form className="form">
-
-
-				<input
-				
-					type="text"
-					className="form-input"
-					placeholder='Top Text'
-
-				
-				/> 
-				<input
-				
-				type="text"
-				className="form-input"
-				placeholder='Bottom Text'
-
-				/>
-	<button className='form--button'
-	
-	
-	
-	>
-		    Get a new meme image 🖼
-
-	</button>
-
-
+				<input type="text" className="form-input" placeholder="Top Text" />
+				<input type="text" className="form-input" placeholder="Bottom Text" />
+				<button className="form--button" onClick={getMemeImage}>
+					Get a new meme image 🖼
+				</button>
 			</form>
-
-
-
 		</main>
 	)
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Header from '../components/Header'
 import Meme from '../components/Meme'
+import memesData from '../memesData'
 import './App.css'
 
 function App() {
