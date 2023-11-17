2d65b1543fbd289a89ad936418c0f22222aa775a
Kacper <kacpercybulski@icloud.com>
2023-11-17 18:22:11 +0100

    failed on the task that I did yesterday

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -2,44 +2,21 @@ import React from 'react'
 import { useState } from 'react'
 import memesData from '../memesData.jsx'
 
+/**
+ * Lastly, update the `getMemeImage` function and the markup
+ * to reflect our newly reformed state object and array in the
+ * correct way.
+ */
+
 function Meme() {
-	/**
-	 * Challenge: Update our state to save the meme-related
-	 * data as an object called `meme`. It should have the
-	 * following 3 properties:
-	 * topText, bottomText, randomImage.
-	 *
-	 * The 2 text states can default to empty strings for now,
-	 * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
-	 *
-	 * Next, create a new state variable called `allMemeImages`
-	 * which will default to `memesData`, which we imported above
-	 *
-	 * Lastly, update the `getMemeImage` function and the markup
-	 * to reflect our newly reformed state object and array in the
-	 * correct way.
-	 */
-
-	// const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
-
-	const [meme, setMeme] = useState({
-		topText: '',
-		bottomText: '',
-		randomImage: 'http://i.imgflip.com/1bij.jpg',
-	})
-
-	const [allMemeImages, setAllMemeImages] = useState(memesData)
+	const [memeImage, setMemeImage] = React.useState('http://i.imgflip.com/1bij.jpg')
 
 	function getMemeImage() {
-		const memesArray = allMemeImages.data.allMemeImages
+		const memesArray = memesData.data.memes
 		const randomNumber = Math.floor(Math.random() * memesArray.length)
-
-		const url = memesArray[randomNumber].url
-		setMeme({
-			...meme,
-			randomImage: url,
-		})
+		setMemeImage(memesArray[randomNumber].url)
 	}
+
 	return (
 		<main>
 			<div className="form">
@@ -49,7 +26,7 @@ function Meme() {
 					Get a new meme image 🖼
 				</button>
 			</div>
-			<img src={meme.randomImage} className="meme--image" />
+			<img src={memeImage} className="meme--image" />
 		</main>
 	)
 }
