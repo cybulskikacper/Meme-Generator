35ad6c84e9d6bb7577c96f33d891b70c7a04bbfc
Kacper <kacpercybulski@icloud.com>
2023-11-18 16:17:35 +0100

    did the same task I failed on yesterday, Today I did this without any problem

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -3,18 +3,44 @@ import { useState } from 'react'
 import memesData from '../memesData.jsx'
 
 /**
+ * Challenge: Update our state to save the meme-related
+ * data as an object called `meme`. It should have the
+ * following 3 properties:
+ * topText, bottomText, randomImage.
+ *
+ * The 2 text states can default to empty strings for now,
+ * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+ *
+ * Next, create a new state variable called `allMemeImages`
+ * which will default to `memesData`, which we imported above
+ *
  * Lastly, update the `getMemeImage` function and the markup
  * to reflect our newly reformed state object and array in the
  * correct way.
  */
 
 function Meme() {
-	const [memeImage, setMemeImage] = React.useState('http://i.imgflip.com/1bij.jpg')
+	// const [memeImage, setMemeImage] = React.useState('http://i.imgflip.com/1bij.jpg')
+
+	const [meme, setMeme] = useState({
+		topText: '',
+		bottomText: '',
+		randomImage: 'http://i.imgflip.com/1bij.jpg',
+	})
+
+	const [allMemeImages, setAllMemeImages] = useState(memesData)
 
 	function getMemeImage() {
-		const memesArray = memesData.data.memes
+		const memesArray = allMemeImages.data.memes
 		const randomNumber = Math.floor(Math.random() * memesArray.length)
-		setMemeImage(memesArray[randomNumber].url)
+		const url = memesArray[randomNumber].url
+
+		setMeme(prevMeme => {
+			return {
+				...meme,
+				randomImage: url,
+			}
+		})
 	}
 
 	return (
@@ -26,7 +52,7 @@ function Meme() {
 					Get a new meme image 🖼
 				</button>
 			</div>
-			<img src={memeImage} className="meme--image" />
+			<img src={meme.randomImage} className="meme--image" />
 		</main>
 	)
 }
